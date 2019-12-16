c9f70d2d4d79761690f53523e31c912512d7c0bc
Lakshmanan <835156@cognizant.com>
2019-12-16 17:18:39 +0530

    webui

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,26 +1,26 @@
 function valid() {
-    var name = document.forms["form"]["name"].value;
-    var boxoffice = document.forms["form"]["boxoffice"].value;
-    var dateofLaunch = document.forms["form"]["dateofLaunch"].value;
+    var title = document.forms["form"]["title"].value;
+    var boxOffice = document.forms["form"]["boxOffice"].value;
+    var dateOfLaunch = document.forms["form"]["dateOfLaunch"].value;
     var genre = document.forms["form"]["genre"].value;
 
-    if (name == "") {
+    if (title == "") {
         alert("Title is required. ");
         return false;
-    } else if (name.length < 2 || name.length > 100) {
+    } else if (title.length < 2 || title.length > 100) {
         alert("Title should have 2 to 100 characters.");
         return false;
     }
 
-    else if (boxoffice == "") {
+    else if (boxOffice == "") {
         alert("Box Office is required. ");
         return false;
-    } else if (isNaN(boxoffice)) {
+    } else if (isNaN(boxOffice)) {
         alert("Box Office has to be a number.");
         return false;
     }
 
-    else if (dateofLaunch == "") {
+    else if (dateOfLaunch== "") {
         alert("Date of Launch is required. ");
         return false;
     }
